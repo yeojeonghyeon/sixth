166fd99307b67f2d56c1e9eb73abb335120f8108
heygirl <yeojeonghyeon@gmail.com>
2020-07-21 15:53:58 +0900

    20200721_01

--- java/HelloWeb/src/com/helloworld/servlet/ChainedServlet.java
+++ java/HelloWeb/src/com/helloworld/servlet/ChainedServlet.java
@@ -21,22 +21,30 @@ public class ChainedServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String menuType = request.getParameter("menuType");
-		System.out.println(menuType);
 		List<MenuItem> list = new ArrayList<MenuItem>();
-		list.add(new MenuItem("1000", "", "전라남도"));		
-		list.add(new MenuItem("2000", "", "전라북도"));
-		list.add(new MenuItem("1001", "1000", "여수시"));
-		list.add(new MenuItem("1002", "1000", "순천시"));
-		list.add(new MenuItem("2001", "2000", "정읍시"));
-		list.add(new MenuItem("2101", "2001", "삼례"));
-		list.add(new MenuItem("2002", "2000", "전주시"));
-		list.add(new MenuItem("1101", "1001", "소호동"));
-		list.add(new MenuItem("11011", "1101", "소호동 1길"));
-		list.add(new MenuItem("1102", "1001", "종화동"));
-		list.add(new MenuItem("1201", "1002", "조례동"));
-		list.add(new MenuItem("1202", "1002", "연향동"));
-		list.add(new MenuItem("1211", "1202", "연향 1길"));
-		list.add(new MenuItem("1212", "1202", "연향 2길"));
+		if ( menuType.equals("address") ) {
+			list.add(new MenuItem("1000", "", "전라남도"));		
+			list.add(new MenuItem("2000", "", "전라북도"));
+			list.add(new MenuItem("1001", "1000", "여수시"));
+			list.add(new MenuItem("1002", "1000", "순천시"));
+			list.add(new MenuItem("2001", "2000", "정읍시"));
+			list.add(new MenuItem("2101", "2001", "삼례"));
+			list.add(new MenuItem("2002", "2000", "전주시"));
+			list.add(new MenuItem("1101", "1001", "소호동"));
+			list.add(new MenuItem("11011", "1101", "소호동 1길"));
+			list.add(new MenuItem("1102", "1001", "종화동"));
+			list.add(new MenuItem("1201", "1002", "조례동"));
+			list.add(new MenuItem("1202", "1002", "연향동"));
+			list.add(new MenuItem("1211", "1202", "연향 1길"));
+			list.add(new MenuItem("1212", "1202", "연향 2길"));
+		}else {
+			list.add(new MenuItem("1000", "", "커피"));	
+			list.add(new MenuItem("1100", "1000", "원두커피"));				
+			list.add(new MenuItem("1200", "1000", "인스턴트커피"));				
+			list.add(new MenuItem("1101", "1200", "맥심"));			
+			list.add(new MenuItem("1102", "1200", "카누"));			
+			list.add(new MenuItem("2000", "", "음료"));
+		}
 		Gson gson = new Gson();
 		response.setContentType("plain/text;charset=utf-8");
 		response.getWriter().append(gson.toJson(list));
